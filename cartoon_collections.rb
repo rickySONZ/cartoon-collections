46da40140a823758a6625de75841132a8c77f94a
Ryan Erricson <ryanerricson@gmail.com>
2021-02-04 10:41:16 -0500

    commit tree 5e38478cd07f7342b169fa1d4f691d1ba3441aa4

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |name, index|
+    puts "#{index+ 1}. #{name}"
+
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|planet| planet.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  answer = false
+  if array.any?{|i| i.length > 4}
+    answer = true
+  end
+  answer
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect{|i| cheese_types.include?(i)}
 end
